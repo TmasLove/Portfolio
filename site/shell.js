1c3ec08681e842b145c8508866c40645f157d072
Tommy <troldan92@gmail.com>
2026-09-08 11:21:36 -0400

    Phone boots with the About sheet open too

--- site/shell.js
+++ site/shell.js
@@ -568,7 +568,7 @@ document.addEventListener('keydown',function(e){
 });
 
 /* boot: About opens on a fresh login (Tommy: "have it land on About instead of Work") */
-if(!isPhone())setTimeout(openAbout,reduced?0:260);
+setTimeout(function(){if(isPhone())openSheetPage('about');else openAbout()},reduced?0:260);
 window.trOpen=openApp;
 window.TR={esc:esc,sqHTML:sqHTML,ART:ART,APPS:APPS,DOCK:DOCK,PAGES:PAGES,ME:ME,openMap:openMap,createWindow:createWindow,closeWindow:closeWindow,minimize:minimize,restore:restore,topWin:topWin,openApp:openApp,openPage:openPage,openLP:openLP,openSpot:openSpot,closeMenus:closeMenus,isPhone:isPhone,register:function(key,title,open,sheet){REG[key]={title:title,open:open,sheet:sheet}}};
 })();
